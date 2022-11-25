51a539b5ea49c495865d6ab3fe28d9e4a7662954
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-11-25 23:57:36 +0400

    conditions switch case

---
+++ Conditions/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Conditions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 8;
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+
+            var t = a > b ? a : b;
+            Console.WriteLine(t);
+
+            switch (a)
+            {
+                case 2:
+                    {
+                        Console.WriteLine(a);
+                        break;
+                    };
+                case 3:
+                    {
+                        Console.WriteLine(a);
+                        break;
+                    };
+
+                case 4:
+                    {
+                        Console.WriteLine(a);
+                        break;
+                    };
+
+                case 5:
+                    {
+                        Console.WriteLine(a);
+                        break;
+                    };
+
+
+
+            }
+        }
+    }
+}
