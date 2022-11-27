56951c203d743a969470843a73b00935dafec8be
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-11-27 21:29:39 +0400

    test METHODS

---
+++ Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+        }
+        public static int TestInt()
+        {
+            Console.WriteLine("TestInt");
+            return 0;        
+        }
+        public static double TestDouble()
+        {
+            Console.WriteLine("TestInt");
+            return 0;
+        }
+    }
+}
