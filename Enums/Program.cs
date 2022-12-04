79add29a518b83466d81059ca756fbb79862d48d
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-12-04 23:40:13 +0400

    Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Enums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(TestEnum.Abovyan);
+            Console.WriteLine(TestEnum.Abovyan.ToString());
+            Console.WriteLine((int)TestEnum.Abovyan);
+
+        }
+        public enum TestEnum
+        {
+            Yerevan,
+            Gyumri,
+            Vanadzor,
+            Ejmiacin,
+            Abovyan,
+            Dilijan,
+            Ijevan,
+            Gavar,
+            Goris,
+            Kapan,
+            Sisian,
+            Hrazdan,
+            Artashat,
+            Armavir,
+            Sevan,
+            Charencavan,
+            Masis,
+            Ararat,
+            Ashtarak,
+            Artik,
+            Tsaxkadzor
+        }
+    }
+}
