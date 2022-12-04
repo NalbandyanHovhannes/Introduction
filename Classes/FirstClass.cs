9a3f6ad4b3e94cb4dcbc2ec5f26ce42efe07305d
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-12-05 00:52:48 +0400

    Classes

---
+++ Classes/FirstClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public  class FirstClass
+    {
+        int t = 8;
+        public int FirstMethod(int u)
+        {
+            return u;
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FirstClass firstClass = new FirstClass();
+            firstClass.FirstMethod(9);            
+
+        }
+    }
+}
