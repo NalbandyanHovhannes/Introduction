ddcc0b0886fd6ca367353bafd470a1cbc84d002d
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-12-08 00:18:29 +0400

    .

--- Structs/TestStruct.cs
+++ Structs/TestStruct.cs
@@ -2,9 +2,16 @@
 {
     public struct TestStruct
     {
+        int t;
         public void TestMethod()
         {
+            String
             Console.WriteLine("TestMethod in Struct");
         }
+        public TestStruct(int y)
+        {
+            t = y;
+        }
+      
     }
 }
