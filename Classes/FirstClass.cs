7147ada7c733dca51c6d729f9d71048ac0947312
NalbandyanHovhannes <Nalbandyan.hovh@gmail.com>
2022-12-10 23:57:05 +0400

    Classed Done

--- Classes/FirstClass.cs
+++ Classes/FirstClass.cs
@@ -1,23 +1,25 @@
 ﻿namespace Classes
 {
-    public  class FirstClass
+    public class FirstClass
     {
-       public int v;
-      public  int a;
+        public int v;
+        public int a;
         public static void MyMethod(int t)
         {
             Console.WriteLine(t);
 
         }
+
+        //Default Constructor
         public FirstClass()
         {
 
         }
+        //Constructor with parameter // its like method overloading 
         public FirstClass(int u)
         {
-            
-            v = u; 
+            v = u;
             MyMethod(u);
         }
-    } 
+    }
 }
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,13 +4,12 @@
     {
         static void Main(string[] args)
         {
-            FirstClass firstClass0;
-            FirstClass firstClass = new FirstClass();
-            FirstClass firstClass1 = new FirstClass(8);
-            FirstClass.MyMethod(9);
-            FirstClass firstClass11 = new FirstClass(8);
-            firstClass.a = 1;
-
+            FirstClass firstClass0;//
+            FirstClass firstClass = new FirstClass();          // object with default constructor
+            FirstClass firstClass1 = new FirstClass(8);       // object with parametr constructor 
+            FirstClass.MyMethod(9);                          // Static method called in class
+            FirstClass firstClass11 = new FirstClass(8);    // object with parametr constructor 
+            firstClass.a = 1;                              // set field value on class
         }
     }
 }
